8466f6a463442b6a22296367d5c7c1175974ced7
default <15580ogu@wcdsbcloud.ca>
2025-09-22 23:40:11 +0000

    Added block comments and created main.ts with 'I love computer science'

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Faridah Ogunleye\
+ * @version 1.0.0
+ * @date 2025-09-22
+ * @fileoverview This program practices with string literals
+ */
+
+// string literal text
+console.log("I love Computer Science! !");
+
+// string literal text
+console.log("    I  love Computer Science");
+
+// string literal text
+console.log("I   love   Computer   Science   100%");
+
+console.log("\nDone.");
